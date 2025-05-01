fce8b9798b5539c4655c553c2bc4705d2ac5a631
Francesco Nicolo <nicolo.francesco.fn@gmail.com>
2025-05-01 12:00:32 +0200

    cambiato colore scritti layout

--- src/shared/components/templates/TemplateBasePage.tsx
+++ src/shared/components/templates/TemplateBasePage.tsx
@@ -6,8 +6,10 @@ export const TemplateBasePage: FC<PropsWithChildren> = ({ children }) => {
   const { t } = useTranslation();
   return (
     <>
-      <div className="h-10 w-full bg-primary text-center absolute top-0 left-0">
-        <TypographyH3>{t('title-layout')}</TypographyH3>
+      <div className="h-10 w-full bg-foreground text-center absolute top-0 left-0">
+        <TypographyH3 className="text-background">
+          {t('title-layout')}
+        </TypographyH3>
       </div>
       {children}
     </>
